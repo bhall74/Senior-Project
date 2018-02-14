2a3aae812e92ded3e1b1041ad1d37b976bf0264b
Bryan Hall <bhall1224@gmail.com>
2018-02-14 17:21:05 -0500

    Initial Commit

---
+++ adventure_creator_skill/src/main/java/ac/alexa/speechlet/App.java
@@ -0,0 +1,13 @@
+package ac.alexa.speechlet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
