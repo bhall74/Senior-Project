fc0c1c54a7a3febf4ece5af64175c4f590a11ab2
Bryan Hall <bhall1224@gmail.com>
2018-02-23 13:58:22 -0500

    Added directory for Android component. Has template

---
+++ adventure_android_app/app/src/test/java/team12/cs4850/com/adventurecreator/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package team12.cs4850.com.adventurecreator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
